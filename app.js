bec2f25bc15c875d4896ef779c7ce0100aa6dede
Konrad4q <125660510+Konrad4q@users.noreply.github.com>
2023-12-01 19:44:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        if (isNaN(result) || !isFinite(result)) {
+            throw new Error('Invalid result');
+        }
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
